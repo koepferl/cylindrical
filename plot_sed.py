611ac47f06521e85df3ed8e66c8bf4b8f9fa13a6
koepferl <koepferl@mpia.de>
2012-10-25 13:44:06 +0200

    added stuff

--- plot_sed.py
+++ plot_sed.py
@@ -16,7 +16,7 @@ ax = fig.add_subplot(1, 1, 1)
 # scale to 300pc. In Python, negative indices can be used for lists and
 # arrays, and indicate the position from the end. So to get the SED in the
 # largest aperture, we set aperture=-1.
-wav, nufnu = m.get_sed(inclination=9, aperture=-1, distance=300 * pc)
+wav, nufnu = m.get_sed(inclination=0, aperture=-1, distance=300 * pc)
 
 # Plot the SED. The loglog command is similar to plot, but automatically
 # sets the x and y axes to be on a log scale.
--- temp-test_disc.py
+++ temp-test_disc.py
@@ -10,7 +10,7 @@ m = ModelOutput('test_disc.rtout')
 # Extract the quantities
 g = m.get_quantities()
 
-# Get the wall positions for r and theta
+# Get the wall positions for r and z
 rw, zw = g.w_wall / au, g.z_wall / au
 
 print rw, zw
@@ -26,29 +26,30 @@ ax.set_xscale('log')
 ax.set_xlim(rw[1], rw[-1])
 ax.set_ylim(zw[0], zw[-1])
 ax.set_xlabel('r (au)')
-ax.set_ylabel('z')
-ax.set_yticks([np.pi, np.pi*0.75, np.pi * 0.5, np.pi * 0.25, 0.])
-ax.set_yticklabels([r'$\pi$', r'$3\pi/4$', r'$\pi/2$',r'$\pi/4$', r'$0$'])
+ax.set_ylabel('z (au)')
+ax.set_yticks([-300, -200, -100, 0, 100, 200, 300])
+ax.set_yticklabels(['-300', '-200','-100','0', '100', '200','300'])
 cb = fig.colorbar(c)
 cb.set_label('Temperature (K)')
 fig.savefig('temperature_spherical_rt.png', bbox_inches='tight')
 
 # Calculate the position of the cell walls in cartesian coordinates
-R, T = np.meshgrid(rw, tw)
-X, Z = R * np.sin(T), R * np.cos(T)
+#R, T = np.meshgrid(rw, tw)
+#X, Z = R * np.sin(T), R * np.cos(T)
+X=R
 
 # Make a plot in (x, z) space for different zooms
 fig = plt.figure(figsize=(16, 8))
 
 ax = fig.add_axes([0.1, 0.1, 0.2, 0.8])
-c = ax.pcolormesh(X, Z, g['temperature'][0].array[0, :, :], vmin=0, vmax=100)
+c = ax.pcolormesh(X, Z, g['temperature'][0].array[0, :, :],vmin=0, vmax=100)
 ax.set_xlim(X.min(), X.max())
 ax.set_ylim(Z.min(), Z.max())
 ax.set_xlabel('x (au)')
 ax.set_ylabel('z (au)')
 
 ax = fig.add_axes([0.32, 0.1, 0.2, 0.8])
-c = ax.pcolormesh(X, Z, g['temperature'][0].array[0, :, :], vmin=0, vmax=100)
+c = ax.pcolormesh(X, Z, g['temperature'][0].array[0, :, :],vmin=0, vmax=100)
 ax.set_xlim(X.min() / 10., X.max() / 10.)
 ax.set_ylim(Z.min() / 10., Z.max() / 10.)
 ax.set_xlabel('x (au)')
@@ -56,7 +57,7 @@ ax.set_yticklabels('')
 ax.text(0.1, 0.95, 'Zoom 10x', ha='left', va='center', transform=ax.transAxes, color='white')
 
 ax = fig.add_axes([0.54, 0.1, 0.2, 0.8])
-c = ax.pcolormesh(X, Z, g['temperature'][0].array[0, :, :], vmin=0, vmax=100)
+c = ax.pcolormesh(X, Z, g['temperature'][0].array[0, :, :],vmin=0, vmax=100)
 ax.set_xlim(X.min() / 100., X.max() / 100)
 ax.set_ylim(Z.min() / 100, Z.max() / 100)
 ax.set_xlabel('x (au)')
